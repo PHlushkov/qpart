6acf81fa9b728433138d8223e4169c04696887c4
Maxim <rudajtes@gmail.com>
2024-11-07 12:26:05 +0200

    first commit

---
+++ src/pages/authorization/Authorization.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Authorization() {
+    return (
+        <div>
+            <h1>Autorization</h1>
+        </div>
+    )
+}
+
+export default Authorization
---
+++ src/pages/machines/Machines.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Machines() {
+    return (
+        <div>
+            <h1>Machines</h1>
+        </div>
+    )
+}
+
+
+export default Machines;
