8ea3b2e99b84b977180b7b64b9d851c22ec8af8f
PHlushkov <pavelgluskov264@gmail.com>
2024-11-12 00:32:24 +0300

    refactor routing

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,15 +1,22 @@
+import React from 'react';
+import { Route, Routes, Navigate } from 'react-router';
 import Authorization from 'pages/authorization/Authorization';
 import Home from 'pages/home/Home';
 import Machines from 'pages/machines/Machines';
 import NotFound from 'pages/notFound/NotFound';
 import ReportPage from 'pages/report/ReportPage';
-import React from 'react';
-import { Route, Routes, Navigate } from 'react-router';
 
 interface AppRoutesProps {
   isAuthenticated: boolean;
   onLogin: () => void;
 }
+
+const useAuthenticatedRoute = (
+  isAuthenticated: boolean,
+  route: React.ReactNode,
+) => {
+  return isAuthenticated ? route : <Navigate to="/authorization" />;
+};
 
 const AppRoutes: React.FC<AppRoutesProps> = ({ isAuthenticated, onLogin }) => {
   return (
@@ -18,31 +25,22 @@ const AppRoutes: React.FC<AppRoutesProps> = ({ isAuthenticated, onLogin }) => {
         path="/authorization"
         element={<Authorization onLogin={onLogin} />}
       />
-
       <Route
         path="/home"
-        element={isAuthenticated ? <Home /> : <Navigate to="/authorization" />}
+        element={useAuthenticatedRoute(isAuthenticated, <Home />)}
       />
-
       <Route
         path="/reports"
-        element={
-          isAuthenticated ? <ReportPage /> : <Navigate to="/authorization" />
-        }
+        element={useAuthenticatedRoute(isAuthenticated, <ReportPage />)}
       />
-
       <Route
         path="/machines"
-        element={
-          isAuthenticated ? <Machines /> : <Navigate to="/authorization" />
-        }
+        element={useAuthenticatedRoute(isAuthenticated, <Machines />)}
       />
-
       <Route
         path="/"
         element={<Navigate to={isAuthenticated ? '/home' : '/authorization'} />}
       />
-
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
